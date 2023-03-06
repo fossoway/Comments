d4d8229e620a246c55bb598791c0a4a17298bbea
Elena Bukina <selenaastu@gmail.com>
2023-03-06 12:24:38 +0400

    add createElements

---
+++ js/createElements.js
@@ -0,0 +1,33 @@
+export const createComment = ({name, date, comment}) => {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('comments__wrapper');
+
+  const header = document.createElement('div');
+  header.classList.add('comments__header');
+
+  const userName = document.createElement('p');
+  userName.classList.add('comments__name');
+  userName.textContent = name;
+
+  const dateElem = document.createElement('p');
+  dateElem.classList.add('comments__date');
+  dateElem.textContent = date;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.type = 'button';
+  deleteBtn.classList.add('comments__button', 'comments__button-delete');
+
+  header.append(userName, dateElem, deleteBtn);
+
+  const textComment = document.createElement('p');
+  textComment.classList.add('comments__text');
+  textComment.textContent = comment;
+
+  const likeBtn = document.createElement('button');
+  likeBtn.type = 'button';
+  likeBtn.classList.add('comments__button', 'comments__button-delete');
+
+  wrapper.append(header, textComment,likeBtn);
+
+  return wrapper;
+};

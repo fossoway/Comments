1e9094b22f58198a4709d3806be8a9d3c511bb20
Elena Bukina <selenaastu@gmail.com>
2023-03-07 17:46:38 +0400

    update validation

--- js/control.js
+++ js/control.js
@@ -1,25 +1,72 @@
-import { removeStorage, setStorage, editStorage } from "./storage.js";
-import { addComment } from "./render.js";
+import {removeStorage, setStorage, editStorage} from "./storage.js";
+import {addComment} from "./render.js";
+
+
+const addAlert = (text) => {
+  const alert = document.createElement('p');
+  alert.classList.add('alert');
+  alert.innerText = text;
+  return alert;
+}
+
+
+const nameValidation = (elem) => {
+  const regExpName = /[^а-яёА-ЯЁ]/u;
+  elem.addEventListener('input', e => {
+    if (regExpName.test(elem.value)) {
+      if (!elem.nextElementSibling) {
+        const alert = addAlert(`Только кириллица!`);
+        elem.parentElement.append(alert);
+      }
+    }
+  })
+}
+
+
+const isEmptyValidation = (elem) => {
+  elem.addEventListener('blur', e => {
+    if (elem.value.length === 0) {
+      const alert = addAlert(`Поле не заполнено!`);
+      elem.parentElement.append(alert);
+    }
+  })
+};
+
+
+const deleteError = (elem) => {
+  elem.addEventListener('input', e => {
+    if (elem.nextElementSibling) {
+      elem.nextElementSibling.remove();
+    }
+  })
+};
 
 
 const commentFormControl = (form, elem, localStorageKey) => {
+  const elements = form.querySelectorAll('.form__validation');
+  const name = form.querySelector('#name');
+  elements.forEach(item => isEmptyValidation(item));
+  elements.forEach(item => deleteError(item));
+  nameValidation(name);
   const submitComment = (event) => {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    const newComment = Object.fromEntries(formData);
-    newComment.id = Math.round(Math.random() * 100000).toString();
-    newComment.isLike = 'no';
-    console.log(newComment);
-    if (!newComment.date) {
-      const today = new Date();
-      newComment.date = today.toLocaleString();
+    const alert = form.querySelector('.alert');
+    if (!alert) {
+      const formData = new FormData(event.target);
+      const newComment = Object.fromEntries(formData);
+      newComment.id = Math.round(Math.random() * 100000).toString();
+      newComment.isLike = 'no';
+      if (!newComment.date) {
+        const today = new Date();
+        newComment.date = today.toLocaleString();
+      }
+      setStorage(localStorageKey, newComment);
+      addComment(newComment, elem);
+      form.reset();
     }
-    setStorage(localStorageKey, newComment);
-    addComment(newComment, elem);
-    form.reset();
-  };
+  }
+
   form.addEventListener('submit', submitComment);
-  form.addEventListener('enter', submitComment)
 };
 
 
@@ -46,4 +93,4 @@ const deleteComment = (app, localStorageKey) => {
 }
 
 
-export { commentFormControl, addLike, deleteComment };
+export {commentFormControl, addLike, deleteComment};
